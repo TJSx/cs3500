598731b2390b209dc3d2c76f0061b644de6584e9
Tamara Spivey <tspivey606@gmail.com>
2019-03-19 13:34:58 -0500

    Update SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -15,6 +15,23 @@ public:
   //Constructor
   SYMBOL_TABLE( ) { }
 
+  int getEntryTypeST(string theName)
+  {
+    return hashTable[theName].getTheType();     // defn STEntry.h: int getTheType() const {return type_struct.type;}
+  }
+  
+    // Find symbol table entry index with theName, return numParams
+  int getNumParamsST(string theName)
+  {
+    return hashTable[theName].getNumParams();
+  }
+
+  // Find symbol table entry index with theName, return returnType
+  int getReturnTypeST(string theName)
+  {
+    return hashTable[theName].getReturnType();
+  }
+  
   // Add SYMBOL_TABLE_ENTRY x to this symbol table.
   // If successful, return true; otherwise, return false.
   bool addEntry(SYMBOL_TABLE_ENTRY x)
@@ -28,7 +45,10 @@ public:
     }
     else return(false);
   }
-
+  std::map<string, SYMBOL_TABLE_ENTRY> getHashTable()
+  {
+	 return hashTable; 
+  }
   // If a SYMBOL_TABLE_ENTRY with name theName is
   // found in this symbol table, then return true;
   // otherwise, return false.
