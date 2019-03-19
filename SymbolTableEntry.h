23cac3883db58149fd210339ccfa04da6bd81bfd
Tamara Spivey <tspivey606@gmail.com>
2019-03-19 13:32:20 -0500

    Update SymbolTableEntry.h

--- SymbolTableEntry.h
+++ SymbolTableEntry.h
@@ -5,27 +5,55 @@
 using namespace std;
 
 #define UNDEFINED  -1
+#define FUNCTION 0
+#define INT 1
+#define STR 2
+#define FLOAT 3
+#define LIST 4
+#define NULL 5
+
+#define NOT_APPLICABLE -2
+
+typedef struct
+{
+  int type;
+  int numParams;
+  int returnType;
+}TYPE_INFO
 
 class SYMBOL_TABLE_ENTRY
 {
 private:
   // Member variables
+  TYPE_INFO type_struct;
   string name;
-  int typeCode;
 
 public:
   // Constructors
-  SYMBOL_TABLE_ENTRY( ) { name = ""; typeCode = UNDEFINED; }
+  SYMBOL_TABLE_ENTRY( ) 
+  { 
+	  name = ""; 
+    type_struct.type = NOT_APPLICABLE; 
+    type_struct.numParams = NOT_APPLICABLE;
+    type_struct.returnType = NOT_APPLICABLE;
+}
 
-  SYMBOL_TABLE_ENTRY(const string theName, const int theType)
+  SYMBOL_TABLE_ENTRY(const string theName, const int theType, const int numParams, const int returnType)
   {
     name = theName;
-    typeCode = theType;
+   // typeCode = theType;
+    type_struct.type = NOT_APPLICABLE; 
+    type_struct.numParams = NOT_APPLICABLE;
+    type_struct.returnType = NOT_APPLICABLE;
   }
 
   // Accessors
+  int getNumParams() const {return type_struct.numParams;}
+  int getReturnType() const{return type_struct.returnType;}
+  int getTheType() const {return type_struct.type;}
+  
   string getName() const { return name; }
-  int getTypeCode() const { return typeCode; }
+  TYPE_INFO getTypeStruct() const { return type_struct; }
 };
 
 #endif  // SYMBOL_TABLE_ENTRY_H
